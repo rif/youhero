9c71f24ede710f2193957d2cc124971c9e8bb10a
Radu Ioan Fericean <radu@fericean.ro>
2012-04-26 16:30:37 +0300

    advanced working

--- gdata/youtube.go
+++ gdata/youtube.go
@@ -38,42 +38,40 @@ func ParseEntry(entry string) (ye *YEntry, err error) {
 	if url := urlRe.FindStringSubmatch(entry); len(url) == 2 {
 		ye.Url = url[1]
 	} else {
-		err = errors.New("Malformed entry")
+		err = errors.New("Malformed entry: url")
 	}
 	if thumb := thumbRe.FindStringSubmatch(entry); len(thumb) == 2 {
 		ye.Thumb = thumb[1]
 	} else {
-		err = errors.New("Malformed entry")
+		err = errors.New("Malformed entry: thumb")
 	}
 	if title := titleRe.FindStringSubmatch(entry); len(title) == 2 {
 		ye.Title = html.UnescapeString(title[1])
 	} else {
-		err = errors.New("Malformed entry")
+		err = errors.New("Malformed entry: title")
 	}
 	if description := descriptionRe.FindStringSubmatch(entry); len(description) == 2 {
 		ye.Description = html.UnescapeString(description[1])
 	} else {
-		err = errors.New("Malformed entry")
+		err = errors.New("Malformed entry: description")
 	}
 	if category := categoryRe.FindStringSubmatch(entry); len(category) == 2 {
 		ye.Category = html.UnescapeString(category[1])
 	} else {
-		err = errors.New("Malformed entry")
+		err = errors.New("Malformed entry: category")
 	}
 	if keywords := keywordsRe.FindStringSubmatch(entry); len(keywords) == 2 {
 		ye.Keywords = html.UnescapeString(keywords[1])
 	} else {
-		err = errors.New("Malformed entry")
+		err = errors.New("Malformed entry: keywords")
 	}
 	if published := publishedRe.FindStringSubmatch(entry); len(published) == 2 {
 		ye.Published, err = time.Parse(time.RFC3339, published[1])
 	} else {
-		err = errors.New("Malformed entry")
+		err = errors.New("Malformed entry: published")
 	}
 	if rating := ratingRe.FindStringSubmatch(entry); len(rating) == 3 {
 		ye.Rating = fmt.Sprintf("Rating: %s of 5 stars - %s Votes", rating[1], rating[2])
-	} else {
-		err = errors.New("Malformed entry")
 	}
 	return
 }
@@ -88,7 +86,7 @@ func ParseFeed(feed string, client *http.Client) (yentries []*YEntry, err error)
 	if err == nil {
 		defer resp.Body.Close()
 		body, _ := ioutil.ReadAll(resp.Body)
-		entryRe := regexp.MustCompile(`(?s)<entry>.*?</entry>`)
+		entryRe := regexp.MustCompile(`(?s)<entry.*?>(.*?)</entry>`)
 		entries := entryRe.FindAllString(string(body), 50)
 		for _, e := range entries {
 			if ye, err := ParseEntry(e); err == nil {
